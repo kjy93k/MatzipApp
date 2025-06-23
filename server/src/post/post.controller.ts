41068b968985f802a38972e7accfc958a6976efb
jy.kim <kjy93k@naver.com>
2025-06-23 23:10:55 +0900

    게시글 모듈 CRUD 구현하기

---
+++ server/src/post/post.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { PostService } from './post.service';
+import { CreatePostDto } from './dto/create-post.dto';
+
+@Controller()
+export class PostController {
+  constructor(private postService: PostService) {}
+
+  @Get('/posts')
+  getPost(@Query('page') page: number) {
+    return this.postService.getPosts(page);
+  }
+
+  @Get('/posts/:id')
+  getPostById(@Param('id', ParseIntPipe) id: number) {
+    return this.postService.getPostById(id);
+  }
+
+  @Post('/posts')
+  createPost(@Body() createPostDto: CreatePostDto) {
+    return this.postService.createPost(createPostDto);
+  }
+
+  @Delete('/posts/:id')
+  deletePost(@Param('id', ParseIntPipe) id: number) {
+    return this.postService.deletePost(id);
+  }
+
+  @Patch('posts/:id')
+  @UsePipes(ValidationPipe)
+  updatePost(
+    @Param('id', ParseIntPipe) id: number,
+    @Body()
+    updatePostDto: Omit<CreatePostDto, 'latitude' | 'longitude' | 'address'>,
+  ) {
+    return this.postService.updatePost(id, updatePostDto);
+  }
+}
---
+++ server/src/post/post.service.ts
@@ -0,0 +1,126 @@
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreatePostDto } from './dto/create-post.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Post } from './post.entity';
+
+@Injectable()
+export class PostService {
+  constructor(
+    @InjectRepository(Post)
+    private postRepository: Repository<Post>,
+  ) {}
+
+  getPosts(page: number) {
+    const perPage = 10;
+    const offset = (page - 1) * perPage;
+
+    return this.postRepository
+      .createQueryBuilder('post')
+      .orderBy('post.date', 'DESC')
+      .take(perPage)
+      .skip(offset)
+      .getMany();
+  }
+
+  async getPostById(id: number) {
+    try {
+      const foundPost = await this.postRepository
+        .createQueryBuilder('post')
+        .where('post.id = :id', { id })
+        .getOne();
+      if (!foundPost) {
+        throw new NotFoundException('존재하지 않는 피드입니다.');
+      }
+      return foundPost;
+    } catch (error) {
+      console.log('error', error);
+      throw new InternalServerErrorException(
+        '장소를 가져오는 중 에러가 발생하였습니다.',
+      );
+    }
+  }
+
+  async createPost(createPostDto: CreatePostDto) {
+    const {
+      latitude,
+      longitude,
+      color,
+      address,
+      title,
+      description,
+      date,
+      score,
+      imageUris,
+    } = createPostDto;
+
+    const post = this.postRepository.create({
+      latitude,
+      longitude,
+      color,
+      address,
+      title,
+      description,
+      date,
+      score,
+    });
+
+    try {
+      await this.postRepository.save(post);
+    } catch (error) {
+      console.log('error', error);
+      throw new InternalServerErrorException(
+        '장소를 추가하는 중 에러가 발생하였습니다.',
+      );
+    }
+    return post;
+  }
+
+  async updatePost(
+    id: number,
+    updatePostDto: Omit<CreatePostDto, 'latitude' | 'longitude' | 'address'>,
+  ) {
+    const post = await this.getPostById(id);
+    const { title, description, color, date, score, imageUris } = updatePostDto;
+
+    post.title = title;
+    post.description = description;
+    post.color = color;
+    post.date = date;
+    post.score = score;
+    try {
+      await this.postRepository.save(post);
+    } catch (error) {
+      console.log('error', error);
+      throw new InternalServerErrorException(
+        '장소를 수정하는 중 에러가 발생하였습니다.',
+      );
+    }
+    return post;
+  }
+
+  async deletePost(id: number) {
+    try {
+      const result = await this.postRepository
+        .createQueryBuilder('post')
+        .delete()
+        .from(Post)
+        .where('id = :id', { id })
+        .execute();
+      if (result.affected === 0) {
+        throw new NotFoundException('존재하지 않는 피드입니다.');
+      }
+
+      return id;
+    } catch (error) {
+      console.log('error', error);
+      throw new InternalServerErrorException(
+        '장소를 삭제하는 중 에러가 발생하였습니다.',
+      );
+    }
+  }
+}
