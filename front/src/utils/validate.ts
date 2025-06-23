51c63175208cf0f845b709a3a0cc963bbe4d7cf8
jy.kim <kjy93k@naver.com>
2025-06-23 18:49:23 +0900

    회원가입 validate 처리

--- front/src/utils/validate.ts
+++ front/src/utils/validate.ts
@@ -3,7 +3,7 @@ type UserInformation = {
   password: string;
 };
 
-export const validateLogin = (values: UserInformation) => {
+const validateUser = (values: UserInformation) => {
   const emailReg = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const errors = {
     email: '',
@@ -14,9 +14,25 @@ export const validateLogin = (values: UserInformation) => {
     errors.email = '올바른 이메일 형식이 아닙니다.';
   }
 
-  if (values.password.length >= 8 && values.password.length < 20) {
+  if (!(values.password.length >= 8 && values.password.length < 20)) {
     errors.password = '비밀번호는 8~20자 사이로 입력해주세요.';
   }
-
   return errors;
+};
+
+export const validateLogin = (values: UserInformation) => {
+  return validateUser(values);
+};
+
+export const validateSignup = (
+  values: UserInformation & { passwordConfirm: string },
+) => {
+  const errors = validateUser(values);
+  const signupErrors = { ...errors, passwordConfirm: '' };
+
+  if (values.password !== values.passwordConfirm) {
+    signupErrors.passwordConfirm = '비밀번호가 일치하지 않습니다.';
+  }
+
+  return signupErrors;
 };
--- front/src/screens/auth/SignupScreen.tsx
+++ front/src/screens/auth/SignupScreen.tsx
@@ -1,16 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
+import InputField from '../../components/InputField';
+import useForm from '../../hooks/useForm';
+import { validateSignup } from '../../\butils';
 
 interface SignupScreenProps {}
 
 const SignupScreen = ({}: SignupScreenProps) => {
+  const signup = useForm({
+    initialValue: {
+      email: '',
+      password: '',
+      passwordConfirm: '',
+    },
+    validate: validateSignup,
+  });
+
   return (
-    <View>
-      <Text>회원가입</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <InputField
+          placeholder="이메일"
+          inputMode="email"
+          {...signup.getTextInputProps('email')}
+        />
+        <InputField
+          placeholder="비밀번호"
+          secureTextEntry
+          {...signup.getTextInputProps('password')}
+        />
+        <InputField
+          placeholder="비밀번호 확인"
+          secureTextEntry
+          {...signup.getTextInputProps('passwordConfirm')}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: { flex: 1, margin: 30 },
+  inputContainer: { gap: 20, marginBottom: 30 },
+});
 
 export default SignupScreen;
