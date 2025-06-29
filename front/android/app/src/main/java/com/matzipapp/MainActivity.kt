444833be8b7f5d50ea759746431947f45b68921a
jy.kim <kjy93k@naver.com>
2025-06-29 13:54:30 +0900

    react-native 버전 다운그레이드 관련 수정사항 추가 적용

--- front/android/app/src/main/java/com/matzipapp/MainActivity.kt
+++ front/android/app/src/main/java/com/matzipapp/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.matzipapp
 import com.facebook.react.ReactActivity
-import android.os.Bundle;
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
@@ -19,9 +18,4 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(null)
-  }
 }
-
--- front/src/api/axios.ts
+++ front/src/api/axios.ts
@@ -1,7 +1,13 @@
 import axios from 'axios';
+import { Platform } from 'react-native';
+
+export const baseUrl = {
+  ios: 'http://localhost:3030',
+  android: 'http://10.0.2.2:3030',
+};
 
 const $axios = axios.create({
-  baseURL: 'http://localhost:3030',
+  baseURL: Platform.OS === 'ios' ? baseUrl.ios : baseUrl.android,
   withCredentials: true,
 });
 
